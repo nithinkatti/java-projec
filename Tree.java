bedfec1e1c3cec2da90fc60dd9feb4f399259bcc
nithinkatti <kattinithin8@gmail.com>
2025-06-03 16:23:36 +0530

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,15 @@
+package NEW;
+
+import java.util.*;
+
+public class Tree {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> num = new TreeMap<Integer, String>();
+        num.put(10, "dfc");
+        num.put(20, "ddss");
+        num.put(30, "fefe");
+        num.put(40, "dffx");
+        num.put(50, "fddwd");
+        System.out.println(num);
+    }
+}
