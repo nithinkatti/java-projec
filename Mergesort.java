3aaed7f5b3b5bdb420dc8959c68109ba8051f5be
nithinkatti <kattinithin8@gmail.com>
2025-06-10 10:51:20 +0530

    Add files via upload

---
+++ Mergesort.java
@@ -0,0 +1,73 @@
+package NEW;
+
+import java.util.Arrays;
+
+public class Mergesort {
+
+    // Merge two subarrays of arr[]
+    void merge(int arr[], int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0;
+        int k = left;
+
+        // Merge the temp arrays
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[], if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[], if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main mergeSort function
+    void mergeSort(int arr[], int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // Sort first and second halves
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    // Main method to test the sort
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 4, 1, 3};
+        Mergesort ms = new Mergesort();
+        ms.mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
